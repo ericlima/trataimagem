b93440c88a9e7480c153dcc6b7e048446cf451c5
Eric Lima <eric.george.lima@gmail.com>
2018-04-16 07:06:35 -0300

    ajustes

--- src/trataImagens/Teste.java
+++ src/trataImagens/Teste.java
@@ -8,8 +8,16 @@ public class Teste {
 		
 		try {
 			
-			imagem.processa("/home/eric/cadernos/folhas/folha1.tif");
+			Long initTime = System.nanoTime();
 			
+			imagem.processa("/home/eric/cadernos/folhas/folha5.tif");
+			
+			Long endTime1 = System.nanoTime();
+
+			double seconds = ((double) (endTime1 - initTime)) / 1000000000.0;
+
+			System.out.println("tempo leitura imagem:" + seconds);
+
 			//imagem.apura("p1.jpg");			
 			
 		} catch (Exception e) {
